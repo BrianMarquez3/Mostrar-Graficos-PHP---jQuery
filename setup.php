238b008cef3eef554f5f508a777f3bf55fc16219
BrianMarquez3 <brian3marquez@gmail.com>
2019-12-29 22:14:22 -0500

    Mostrar Datos

---
+++ setup.php
@@ -0,0 +1,10 @@
+<?php
+require_once('lib/idiorm.php');
+$host = 'localhost:3307';
+$user = 'briandb';
+$pass = 'briandb';
+$database = 'php_columnas';
+ORM::configure("mysql:host=$host;dbname=$database");
+ORM::configure('username', $user);
+ORM::configure('password', $pass);
+ORM::configure('driver_options', array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
